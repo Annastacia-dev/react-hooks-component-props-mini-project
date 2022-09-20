d1ec951c25260d866cac02b1901e29738c5d06dd
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-09-20 15:37:16 +0300

    Completed assignment

---
+++ src/components/ArticleList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Article from "./Article"
+
+const ArticleList = ({posts}) => {
+    return (
+      <main>
+        {posts.map(post => {
+          return(
+            <Article key={post.id} {...post}/>
+          )
+        })} 
+        
+      </main>
+    )
+  }
+
+export default ArticleList
